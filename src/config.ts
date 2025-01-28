5b6a03af0445c06517f2b5611d4e8eb9105a0e82
isyizhang <08280828zy@gmail.com>
2025-01-28 16:34:25 +0800

    edit

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Yi Zhang'
-export const SITE_DESCRIPTION = 'Yi Zhang Personal Website '
+export const SITE_TITLE = 'Lyra Yi Zhang'
+export const SITE_DESCRIPTION = 'Lyra Yi Zhang Personal Website '
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
